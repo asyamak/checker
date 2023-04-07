fe0c95a304ad138ccc10790a34d488b886f495de
a.murzagazina <a.murzagazina@andersenlab.com>
2023-04-08 00:58:17 +0600

    remove comments

--- internal/usecase/find_string.go
+++ internal/usecase/find_string.go
@@ -11,31 +11,13 @@ func NewStringFinder() *StringFinder {
 }
 
 func (u *StringFinder) FindMaxSubstring(s string) string {
-	// var result string
-	// for i := 0; i < len(s); i++ {
-	// 	sub := ""
-	// 	for j := i; j < len(s); j++ {
-	// 		if index := strings.IndexByte(sub, s[j]); index == -1 {
-	// 			sub += string(s[j])
-	// 		} else {
-	// 			break
-	// 		}
-	// 	}
-	// 	if utf8.RuneCountInString(sub) > utf8.RuneCountInString(result) {
-	// 		result = sub
-	// 	}
-	// }
-	// return result
 	var maxSubstring string
 	var currentSubstring string
 
-	// Используем хеш-таблицу для отслеживания повторяющихся символов
 	charMap := make(map[rune]bool)
 
-	// Проходим по всей строке, одновременно поддерживая текущую подстроку без повторяющихся символов
 	for _, c := range s {
 		if charMap[c] {
-			// Если символ уже встречался ранее, обрезаем текущую подстроку до следующего индекса после последнего вхождения символа
 			for i, char := range currentSubstring {
 				if char == c {
 					currentSubstring = currentSubstring[i+1:]
@@ -45,11 +27,9 @@ func (u *StringFinder) FindMaxSubstring(s string) string {
 			}
 		}
 
-		// Добавляем символ в текущую подстроку и хеш-таблицу
 		currentSubstring += string(c)
 		charMap[c] = true
 
-		// Если текущая подстрока больше максимальной, обновляем максимальную
 		if len(currentSubstring) > len(maxSubstring) {
 			maxSubstring = currentSubstring
 		}
